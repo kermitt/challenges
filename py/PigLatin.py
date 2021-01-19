112093849f3e8f41155c0547a7cad727c65a1130
Paul Montgomery <paul.montgomery@gmail.com>
2021-01-19 13:11:48 -0800

    prettyier code but passes fewer tests

--- py/PigLatin.py
+++ py/PigLatin.py
@@ -10,42 +10,43 @@ from Tester import verdict, log
 # public static String java_version_of_pig_it(String original) {
 #        return original.replaceAll("(\\w)(\\w*)", "$2$1ay");
 # }
-def pig_it(original):
-    words = list(original.split(" ")) 
-    x = " ".join(map(pigify, words))
-    return x 
 
-def pigify(word):
-    latin = ""
-    punctuation = ""  # maybe a . Maybe a !! Maybe a  !?
-    result = ""
-    for i in range(len(word)):
-        c = word[i]
+def pig_it(text):
+    lst = text.split()
+    return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
 
-        if i == 0:
-            if c.isalnum():
-                latin = c + "ay"
-            else:
-                punctuation = c
-        else:
-            if c.isalnum():
-                result += c
-            else:
-                punctuation += c 
-    return result + latin + punctuation
+# def pig_it_mine(original):
+#     words = list(original.split(" ")) 
+#     x = " ".join(map(pigify, words))
+#     return x 
 
+# def pigify(word):
+#     latin = ""
+#     punctuation = ""  # maybe a . Maybe a !! Maybe a  !?
+#     result = ""
+#     for i in range(len(word)):
+#         c = word[i]
 
-    if len(word) > 1 or word[0].isalnum():
-        result = word[1:len(word)] + word[0] + "ay"        
-    else:
-        result = word
-    return result
+#         if i == 0:
+#             if c.isalnum():
+#                 latin = c + "ay"
+#             else:
+#                 punctuation = c
+#         else:
+#             if c.isalnum():
+#                 result += c
+#             else:
+#                 punctuation += c 
+#     r = result + latin + punctuation
+#     print(r)
+#     return r
 
 if __name__ == '__main__':
 
         verdict(pig_it("Hello world !"),"elloHay orldway !")
         verdict(pig_it("Pig latin is cool"), "igPay atinlay siay oolcay")
         verdict(pig_it("Pig latin."), "igPay atinlay.")
+        verdict(pig_it("yz !"), "zyay !")
         verdict(pig_it("yz!"), "zyay!")
 
 

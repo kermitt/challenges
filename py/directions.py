6d902420673520c45eacbc39f6d741e76f5e3d14
Paul Montgomery <paul.montgomery@gmail.com>
2021-01-20 09:24:34 -0800

    pretty

--- py/directions.py
+++ py/directions.py
@@ -4,18 +4,22 @@ https://www.codewars.com/kata/550f22f4d758534c1100025a/train/python
 from Tester import verdict, log
 
 opposite = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'}
-
-def dirReduc(plan):
-    new_plan = []
-    for d in plan:
-        r = "_"
-        if new_plan and new_plan[-1] == opposite[d]:
-            new_plan.pop()
-            r = "P"
-        else:
-            new_plan.append(d)
+def dirReduc(arr):
+    dir = " ".join(arr)
+    dir2 = dir.replace("NORTH SOUTH",'').replace("SOUTH NORTH",'').replace("EAST WEST",'').replace("WEST EAST",'')
+    dir3 = dir2.split()
+    return dirReduc(dir3) if len(dir3) < len(arr) else dir3
+# def dirReduc(plan):
+#     new_plan = []
+#     for d in plan:
+#         r = "_"
+#         if new_plan and new_plan[-1] == opposite[d]:
+#             new_plan.pop()
+#             r = "P"
+#         else:
+#             new_plan.append(d)
         
-    return new_plan
+#     return new_plan
 
 
 def dirReduc_mine(arr):
